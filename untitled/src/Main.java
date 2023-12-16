13bfa501869c5f38ffdea2acfbfd94d9fe0e17cc
kaan <kaanuzunhan111@hotmail.com>
2023-12-16 04:34:07 +0300

    Vücut kitle hesaplama

---
+++ untitled/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int r;
+        double pi=3.14;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Dairenin yarı çapını giriniz : ");
+        r=input.nextInt();
+
+        double Alan = pi * r * r;
+        double Cevre = 2 * pi * r;
+
+        System.out.println("Dairenin Alanı: " + Alan);
+        System.out.println("Dairenin Cevresi: "+Cevre);
+
+ }
+    }
